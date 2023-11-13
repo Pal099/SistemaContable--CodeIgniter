53e9f6afb623f3a399b3553fb5a83e29f8e020a7
Richar Lin <richard.lin.1801@gmail.com>
2023-11-13 18:46:48 -0300

    2

--- application/models/Pago_obli_model.php
+++ application/models/Pago_obli_model.php
@@ -1,8 +1,9 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Pago_obli_model extends CI_Model
-{
+class Pago_obli_model extends CI_Model {
+	//acá empieza el javorai de isaac
+	//num asi primero
 	public function __construct() {
         $this->load->database();
     }
@@ -176,12 +177,10 @@ public function getUsuarioId($nombre){
 	}
 
 
-
 	public function getDiarios_obli()
 	{
 		$this->db->select('num_asi_deta.*, programa.nombre as nombre_programa,num_asi.FechaEmision as fecha, proveedores.ruc as ruc_proveedor, proveedores.razon_social as razso_proveedor, fuente_de_financiamiento.nombre as nombre_fuente, origen_de_financiamiento.nombre as nombre_origen');
 		$this->db->from('num_asi_deta');
-		$this->db->join('uni_respon_usu', 'programa.id_uni_respon_usu = uni_respon_usu.id_uni_respon_usu');
 		$this->db->join('programa', 'num_asi_deta.id_pro = programa.id_pro');
 		$this->db->join('fuente_de_financiamiento', 'num_asi_deta.id_ff = fuente_de_financiamiento.id_ff');
 		$this->db->join('origen_de_financiamiento', 'num_asi_deta.id_of = origen_de_financiamiento.id_of');
