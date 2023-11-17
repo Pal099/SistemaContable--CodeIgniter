7f8d902a1dae842c96ff73a3c7bc6f86058a4fb7
MaraNaleh <71950203+MaraNaleh@users.noreply.github.com>
2023-11-17 15:24:09 -0300

    Presupuesto (vista)

--- application/views/admin/presupuesto/list.php
+++ application/views/admin/presupuesto/list.php
@@ -1,6 +1,123 @@
-<main id="main" class="main">
-<!-- Content Wrapper. Contains page content -->
-<div class="pagetitle">
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- Agrega estos enlaces en el <head> de tu documento HTML -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+
+    <!-- ... (otros encabezados) ... -->
+    <style>
+        /* Estilos para el contenedor con marco */
+        .content-container {
+            border: 1px solid #ccc;
+            padding: 20px;
+            margin: 10px;
+            border-radius: 10px;
+        }
+
+        /* Estilos para el título de la página */
+        .pagetitle {
+            margin-bottom: 1px;
+            padding-bottom: 10px;
+        }
+
+        /* Estilos para los botones de acciones */
+        .btn-group {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .btn-group .btn {
+            margin-right: 5px;
+        }
+
+        /* Estilo para el contenedor del contenido */
+        .content {
+            background-color: #DCE1FF;
+            /* Cambia el color a tu preferencia */
+            padding: 20px;
+            /* Agrega un espacio interno al contenido para evitar que se superponga con el fondo */
+            color: #000000;
+            /* Cambia el color del texto para que sea legible en el fondo */
+        }
+
+        /* Estilos para los campos opcionales */
+        .optional-fields {
+            display: none;
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-left: 1px;
+            margin-top: 12px;
+            border-radius: 10px;
+        }
+
+        /* Estilos para el switch deslizable */
+        .switch {
+            position: relative;
+            display: inline-block;
+            width: 40px;
+            height: 20px;
+        }
+
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            -webkit-transition: .4s;
+            transition: .4s;
+            border-radius: 20px;
+        }
+
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 16px;
+            width: 16px;
+            left: 2px;
+            bottom: 2px;
+            background-color: white;
+            -webkit-transition: .4s;
+            transition: .4s;
+            border-radius: 50%;
+        }
+
+        input:checked+.slider {
+            background-color: #2196F3;
+        }
+
+        input:focus+.slider {
+            box-shadow: 0 0 1px #2196F3;
+        }
+
+        input:checked+.slider:before {
+            -webkit-transform: translateX(20px);
+            -ms-transform: translateX(20px);
+            transform: translateX(20px);
+        }
+
+        /* Estilos para los campos principales */
+        .main-fields {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 10px;
+        }
+    </style>
+    <!-- En el <head> de tu documento -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+</head>
+<body>
+    <main id="main" class="content">
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-container">
+        <div class="pagetitle">
       <h1>Presupuestos</h1>
       <nav>
         <ol class="breadcrumb">
@@ -17,8 +134,6 @@
           <div class="row">
                     <div class="col-md-12">
                             <a href="<?php echo base_url();?>mantenimiento/presupuesto/add" class="btn btn-primary btn-flat"><span class="fa fa-plus"></span> Agregar presupuesto</a>
-                      
-
                     </div>
                 </div>
                 <hr>
@@ -49,16 +164,15 @@
                                     <th>Diciembre</th>
                                 </tr>
                             </thead>
-    <?php if (!empty($presupuestos) || !empty($cuentacontable) || !empty($programa)|| !empty($registros_financieros) || !empty($origen) ): ?>
-    <?php foreach ($presupuestos as $index => $presupuesto): ?>
-        <tr>
+    <?php if (!empty($presupuestos)|| !empty($cuentacontable) || !empty($programa)|| !empty($registros_financieros) || !empty($origen)): ?>
+    <?php foreach ($presupuestos as $presupuesto): ?>
             <td><?php echo $presupuesto->ID_Presupuesto; ?></td>
             <td><?php echo $presupuesto->Año; ?></td>
-            <td><?php echo $cuentacontable[$index]->Descripcion_CC; ?></td>
+            <td><?php echo $presupuesto->Idcuentacontable; ?></td>
             <td><?php echo $presupuesto->TotalPresupuestado; ?></td>
-            <td><?php echo $origen [$index]->nombre; ?></td>
-            <td><?php echo $registros_financieros [$index]->nombre; ?></td>
-            <td><?php echo $programa [$index]->nombre; ?></td>
+            <td><?php echo $presupuesto->origen_de_financiamiento; ?></td>
+            <td><?php echo $presupuesto->fuente_de_financiamiento; ?></td>
+            <td><?php echo $presupuesto->programa; ?></td>
             <td><?php echo $presupuesto->TotalModificado; ?></td>
             <td><?php echo $presupuesto->pre_ene; ?></td>
             <td><?php echo $presupuesto->pre_feb; ?></td>
@@ -118,3 +232,25 @@
   <!-- /.modal-dialog -->
 </div>
 <!-- /.modal -->
+        </div>
+
+    </main>
+
+    <script>
+        // Manejar la visibilidad de los campos opcionales
+        const optionalFieldsSwitch = document.getElementById("optionalFieldsSwitch");
+        const optionalFields = document.querySelector(".optional-fields");
+
+        optionalFieldsSwitch.addEventListener("change", () => {
+            if (optionalFieldsSwitch.checked) {
+                optionalFields.style.display = "block";
+            } else {
+                optionalFields.style.display = "none";
+            }
+
+
+        });
+    </script>
+</body>
+
+</html>
--- application/views/admin/presupuesto/view.php
+++ application/views/admin/presupuesto/view.php
@@ -1,21 +1,21 @@
 <p><strong>ID presupuesto:</strong> <?php echo $presupuesto->ID_Presupuesto; ?></p>
 <p><strong>Año:</strong> <?php echo $presupuesto->Año; ?></p>
-<p><strong>Descripcion:</strong> <?php echo $presupuesto->Descripcion; ?></p>
-<p><strong>Total Presupuestado:</strong> <?php echo $presupuesto->TotalPresupuestado; ?></p>
+<p><strong>Descripcion:</strong> <?php echo $presupuesto->idcuentacontable; ?></p>
+<p><strong>Total Presupuestado:</strong> <?php echo $presupuesto->$TotalPresupuestado; ?></p>
 <p><strong>Origen de financiamiento:</strong> <?php echo $presupuesto->origen_de_financiamiento_id_of; ?></p>
 <p><strong>Fuente de financiamiento:</strong> <?php echo $presupuesto->fuente_de_financiamiento_id_ff; ?></p>
 <p><strong>Programa:</strong> <?php echo $presupuesto->programa_id_pro; ?></p>
 <p><strong>Total Modificado:</strong> <?php echo $presupuesto->TotalModificado; ?></p>
 <p><strong>Enero:</strong> <?php echo $presupuesto->pre_ene; ?></p>
-<p><strong>Febrero:</strong> <?php echo $presupuesto->pre_ene; ?></p>
-<p><strong>Marzo:</strong> <?php echo $presupuesto->pre_ene; ?></p>
-<p><strong>Abril:</strong> <?php echo $presupuesto->pre_ene; ?></p>
-<p><strong>Mayo:</strong> <?php echo $presupuesto->pre_ene; ?></p>
-<p><strong>Junio:</strong> <?php echo $presupuesto->pre_ene; ?></p>
-<p><strong>Julio:</strong> <?php echo $presupuesto->pre_ene; ?></p>
-<p><strong>Agosto:</strong> <?php echo $presupuesto->pre_ene; ?></p>
-<p><strong>Septiembre:</strong> <?php echo $presupuesto->pre_ene; ?></p>
-<p><strong>Octubre:</strong> <?php echo $presupuesto->pre_ene; ?></p>
-<p><strong>Noviembre:</strong> <?php echo $presupuesto->pre_ene; ?></p>
-<p><strong>Diciembre:</strong> <?php echo $presupuesto->pre_ene; ?></p>
+<p><strong>Febrero:</strong> <?php echo $presupuesto->pre_feb; ?></p>
+<p><strong>Marzo:</strong> <?php echo $presupuesto->pre_mar; ?></p>
+<p><strong>Abril:</strong> <?php echo $presupuesto->pre_abr; ?></p>
+<p><strong>Mayo:</strong> <?php echo $presupuesto->pre_may; ?></p>
+<p><strong>Junio:</strong> <?php echo $presupuesto->pre_jun; ?></p>
+<p><strong>Julio:</strong> <?php echo $presupuesto->pre_jul; ?></p>
+<p><strong>Agosto:</strong> <?php echo $presupuesto->pre_ago; ?></p>
+<p><strong>Septiembre:</strong> <?php echo $presupuesto->pre_sep; ?></p>
+<p><strong>Octubre:</strong> <?php echo $presupuesto->pre_oct; ?></p>
+<p><strong>Noviembre:</strong> <?php echo $presupuesto->pre_nov; ?></p>
+<p><strong>Diciembre:</strong> <?php echo $presupuesto->pre_dic; ?></p>
 
