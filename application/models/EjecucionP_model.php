4ea3901cea5608f51c3f4774c12effeb70774c4d
izok999 <isaacbritez99@gmail.com>
2024-01-04 16:49:32 -0300

    Ejecucion y Presu

--- application/models/EjecucionP_model.php
+++ application/models/EjecucionP_model.php
@@ -3,54 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class EjecucionP_model extends CI_Model {
 
-	public function getSumaDebePorCuenta() {
-		$this->db->select('
-        p.origen_de_financiamiento_id_of,
-        p.fuente_de_financiamiento_id_ff,
-        p.programa_id_pro,
-        p.Idcuentacontable,
-        p.Año,
-        p.TotalPresupuestado,
-        p.TotalModificado,
-        IFNULL(SUM(d.Debe), 0) as Obligado,
-        IFNULL(SUM(d.Haber), 0) as Pagado
-    ');
-    $this->db->from('presupuestos p'); // Asegúrate de que el nombre de la tabla sea correcto
-    $this->db->join('cuentacontable c', 'p.Idcuentacontable = c.IDCuentaContable');
-    $this->db->join('num_asi_deta d', 'c.IDCuentaContable = d.IDCuentaContable', 'left');
-    $this->db->group_by('p.Idcuentacontable, p.ID_Presupuesto');
-    $query = $this->db->get();
-
-    if ($query->num_rows() > 0) {
-        return $query->result();
-    } else {
-        return false;
-    }
-}
-
-	
-
-	public function save($data){
-		return $this->db->insert("ejecucionpresupuestaria",$data);
-	}
-
-	public function getEjecucionP($id){
-		$this->db->where("ID_EjecucionPresupuestaria",$id);
-		$resultado = $this->db->get("ejecucionpresupuestaria");
-		return $resultado->row();
-
-	}
-
-	public function update($id, $data){
-		$this->db->where("ID_EjecucionPresupuestaria",$id);
-		return $this->db->update("ejecucionpresupuestaria",$data);
-	}
-}
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class EjecucionP_model extends CI_Model {
-
 	public function getEjecucionesP($id_uni_respon_usu){
 		$this->db->select("ep.*, p.ID_presupuesto as presupuesto, cc.IDCuentaContable as cuentacontable");
 		$this->db->from("ejecucionpresupuestaria ep");
