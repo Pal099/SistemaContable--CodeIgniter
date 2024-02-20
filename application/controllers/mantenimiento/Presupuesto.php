dd6b1e5aba233b52e58b3bedde8f18d813d1de14
MaraNaleh <71950203+MaraNaleh@users.noreply.github.com>
2024-02-20 16:46:13 -0300

    presupuesto
    suma a excedente

--- application/controllers/mantenimiento/Presupuesto.php
+++ application/controllers/mantenimiento/Presupuesto.php
@@ -11,7 +11,7 @@ class Presupuesto extends CI_Controller {
 	$this->load->model("Presupuesto_model");
 	$this->load->model("Registros_financieros_model");
 	$this->load->model("Origen_model");
-	$this->load->model('Programa_model');
+	$this->load->model('ProgramGasto_model');
 	$this->load->model('Cuentas_model');
 	$this->load->model('CuentaContable_model');
 	$this->load->model('Usuarios_model');
@@ -42,16 +42,26 @@ class Presupuesto extends CI_Controller {
 			'presupuestos' => $this->Presupuesto_model->getPresu($id_uni_respon_usu),
 			'registros_financieros' => $this->Registros_financieros_model->getFuentes($id_uni_respon_usu),
 			'origen' => $this->Origen_model->getOrigenes($id_uni_respon_usu),
-			'programa' => $this->Programa_model->getProgramGastos($id_uni_respon_usu),
-			'ejecucionpresupuestaria' => $this->EjecucionP_model->getEjecucionesP($id_uni_respon_usu),
+			'programa' => $this->ProgramGasto_model->getProgramGastos($id_uni_respon_usu),
+			//'ejecucionpresupuestaria' => $this->EjecucionP_model->getEjecucionesP($id_uni_respon_usu),
 			'cuentacontable'=>$this->CuentaContable_model->getCuentasContables($id_uni_respon_usu),
 		);
-		
+		$mes=1;
+		$idCuentaContable=1;
+		$idOf =1;
+		$idFf=1;
+		$idPro=1;
+
+		$resultado = $this->Presupuesto_model->sumarDebePorMes($mes, $idCuentaContable, $idOf, $idFf, $idPro);
+
+        // Puedes hacer algo con el resultado, como pasarlos a la vista
+        $data['resultado'] = $resultado;
+
+
 		$this->load->view("layouts/header");
-		$this->load->view("layouts/aside");
+		$this->load->view("layouts/sideBar");
 		$this->load->view("admin/presupuesto/list", $data);
 		$this->load->view("layouts/footer");
-
 	}
 
 	public function add(){
@@ -62,17 +72,17 @@ class Presupuesto extends CI_Controller {
 			'presupuesto' => $this->Presupuesto_model->getPresu($id_uni_respon_usu),
 			'registros_financieros' => $this->Registros_financieros_model->getFuentes($id_uni_respon_usu),
 			'origen' => $this->Origen_model->getOrigenes($id_uni_respon_usu),
-			'programa' => $this->Programa_model->getProgramGastos($id_uni_respon_usu),
+			'programa' => $this->ProgramGasto_model->getProgramGastos($id_uni_respon_usu),
 			'cuentacontable' => $this->CuentaContable_model->getCuentasContables($id_uni_respon_usu),
 		);
 
 		$this->load->view("layouts/header");
-		$this->load->view("layouts/aside");
+		$this->load->view("layouts/sideBar");
 		$this->load->view("admin/presupuesto/add", $data);
 		$this->load->view("layouts/footer");
 	}
 
-	public function store(){
+	public function store() {
 		$nombre = $this->session->userdata('Nombre_usuario');
 		$id_user = $this->Usuarios_model->getUserIdByUserName($nombre);
 		$id_uni_respon_usu = $this->Usuarios_model->getUserIdUniResponByUserId($id_user);
@@ -87,8 +97,21 @@ class Presupuesto extends CI_Controller {
 		$mesSeleccionado = $this->input->post('mes');
 		$preMes = $this->input->post("pre_" . $mesSeleccionado);
 	
-		// Crear un array para asignar valores a los campos de mes específicos
-		$meses = array('ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic');
+
+		$excedenteMesAnterior = $this->Presupuesto_model->obtenerDatosPresupuestoAnterior($origen_de_financiamiento_id_of, $fuente_de_financiamiento_id_ff, $programa_id_pro, $descripcion, $mesSeleccionado);
+
+		echo "Excedente del mes anterior: " . $excedenteMesAnterior . "<br>";
+		$debeMesAnterior = $this->Presupuesto_model->sumarDebePorMes($mesSeleccionado, $descripcion, $origen_de_financiamiento_id_of, $fuente_de_financiamiento_id_ff, $programa_id_pro);
+	
+
+		$excedente= $excedenteMesAnterior - $debeMesAnterior;
+	
+		// Sumar el excedente al presupuesto del nuevo mes
+		$preMes += $excedente;
+
+		// Obtener la suma del debe del mes anterior
+	
+		// Construir el array de datos para guardar el nuevo presupuesto
 		$data = array(
 			'Año' => $año,
 			'idcuentacontable' => $descripcion,
@@ -102,11 +125,14 @@ class Presupuesto extends CI_Controller {
 		);
 	
 		// Añadir valores de los campos de mes al array
+		$meses = array('ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic');
 		foreach ($meses as $mes) {
 			$campoMes = "pre_" . $mes;
 			$data[$campoMes] = ($mes == $mesSeleccionado);
 		}
+
 	
+		// Guardar el nuevo presupuesto
 		if ($this->Presupuesto_model->save($data)) {
 			redirect(base_url() . "mantenimiento/presupuesto");
 		} else {
@@ -114,29 +140,69 @@ class Presupuesto extends CI_Controller {
 		}
 	}
 	
+	
+	
+	public function calcularExcedenteInicioMes() {
+		// Obtener la fecha actual
+		$fechaActual = date('Y-m-d');
+		
+		// Obtener el mes y el año actual
+		$mesActual = date('n', strtotime($fechaActual));
+		$anioActual = date('Y', strtotime($fechaActual));
+	
+		// Calcular el mes anterior
+		$mesAnterior = $mesActual - 1;
+		$anioAnterior = $anioActual;
+		if ($mesAnterior == 0) {
+			$mesAnterior = 12;
+			$anioAnterior = $anioActual - 1;
+		}
+	
+		// Obtener el excedente del mes anterior para cada presupuesto
+		// Suponiendo que tengas los datos necesarios para obtener los ID de origen, fuente, programa y cuenta contable
+		$idOf = 1; // Ejemplo de ID de origen de financiamiento
+		$idFf = 1; // Ejemplo de ID de fuente de financiamiento
+		$idPro = 3; // Ejemplo de ID de programa
+		$idCuentaContable = 1; // Ejemplo de ID de cuenta contable
+	
+		// Llamar a la función en el modelo para obtener el excedente del mes anterior
+		$excedenteMesAnterior = $this->Presupuesto_model->obtenerDatosPresupuestoAnterior($idOf, $idFf, $idPro, $idCuentaContable, $mesAnterior);
+	
+		// Sumar el excedente al presupuesto del nuevo mes
+		// Suponiendo que tienes una función en el modelo para actualizar el presupuesto del nuevo mes
+		$this->Presupuesto_model->sumarExcedenteAPresupuestoNuevo($idOf, $idFf, $idPro, $idCuentaContable, $mesActual, $excedenteMesAnterior);
+	
+		// Aquí podrías realizar alguna acción adicional si lo necesitas, como redirigir a otra página o mostrar un mensaje de éxito
+		echo "Excedente del mes anterior calculado y sumado al presupuesto del nuevo mes exitosamente.";
+	}
+	
 	public function edit($id){
+		$nombre = $this->session->userdata('Nombre_usuario');
+		$id_user = $this->Usuarios_model->getUserIdByUserName($nombre);
+		$id_uni_respon_usu = $this->Usuarios_model->getUserIdUniResponByUserId($id_user);
 		$data = array(
 			'presupuesto' => $this->Presupuesto_model->getPresupuesto($id),
-			'registros_financieros' => $this->Registros_financieros_model->getFuentes(),
-			'origen' => $this->Origen_model->getOrigenes(),
-			'programa' => $this->ProgramGasto_model->getProgramGastos(),
-			'cuentacontable'=>$this->CuentaContable_model->getCuentasContables(),
+			'registros_financieros' => $this->Registros_financieros_model->getFuentes($id_uni_respon_usu),
+			'origen' => $this->Origen_model->getOrigenes($id_uni_respon_usu),
+			'programa' => $this->ProgramGasto_model->getProgramGastos($id_uni_respon_usu),
+			'cuentacontable'=>$this->CuentaContable_model->getCuentasContables($id_uni_respon_usu),
 		);
-		$this->load->view("layouts/header");
-		$this->load->view("layouts/aside");
+		//$this->load->view("layouts/header");
+		//$this->load->view("layouts/sideBar");
 		$this->load->view("admin/presupuesto/edit", $data);
-		$this->load->view("layouts/footer");
+		//$this->load->view("layouts/footer");
 	}
 
 	public function update(){
 		$id = $this->input->post("ID_Presupuesto");
-		$anio = $this->input->post("Año");
+		$año = $this->input->post("Año");
 		//$descripcion = $this->input->post("Descripcion");
 		$totalpresupuestado = $this->input->post("TotalPresupuestado");
 		$origen_de_financiamiento_id_of = $this->input->post("origen_de_financiamiento_id_of");
 		$programa_id_pro = $this->input->post("programa_id_pro");
 		$fuente_de_financiamiento_id_ff = $this->input->post("fuente_de_financiamiento_id_ff");
 		$TotalModificado = $this->input->post("TotalModificado");
+		$Idcuentacontable = $this->input->post("Idcuentacontable");
 		$pre_ene = $this->input->post("pre_ene");
 		$pre_feb = $this->input->post("pre_feb");
 		$pre_mar = $this->input->post("pre_mar");
@@ -153,13 +219,14 @@ class Presupuesto extends CI_Controller {
 		$presupuestoactual = $this->Presupuesto_model->getPresupuesto($id);
 		$data = array(
 			'ID_Presupuesto' => $id,
-			'Anio' => $anio,
+			'Año' => $año,
 			//'Descripcion' => $descripcion,
 			'TotalPresupuestado' => $totalpresupuestado,
 			'origen_de_financiamiento_id_of' => $origen_de_financiamiento_id_of,
 			'programa_id_pro' => $programa_id_pro,
 			'fuente_de_financiamiento_id_ff' => $fuente_de_financiamiento_id_ff,
 			'TotalModificado' => $TotalModificado,
+			'Idcuentacontable' => $Idcuentacontable,
 			'pre_ene' => $pre_ene,
 			'pre_feb' => $pre_feb,
 			'pre_mar' => $pre_mar,
@@ -199,6 +266,12 @@ class Presupuesto extends CI_Controller {
 			'estado' => "0",
 		);
 		$this->Presupuesto_model->update($id, $data);
-		echo "mantenimiento/presupuesto";
+		redirect(base_url() . "mantenimiento/presupuesto");
+	
+	}
+
+	public function getPresupuestoDetalle($id) {
+		$presupuestoDetalle = $this->Presupuesto_model->getPresupuesto($id);
+		echo json_encode($presupuestoDetalle);
 	}
 }
--- application/models/Presupuesto_model.php
+++ application/models/Presupuesto_model.php
@@ -1,44 +1,97 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Presupuesto_model extends CI_Model {
-
-
-
-public function getPresu($id_uni_respon_usu) {
-	$this->db->select('pre.*, c.Descripcion_CC as descripcion, ff.nombre as fuente_de_financiamiento, of.nombre as origen_de_financiamiento, pr.nombre as programa');
-	$this->db->from('presupuestos pre');
-	$this->db->join("fuente_de_financiamiento ff", "pre.fuente_de_financiamiento_id_ff = ff.id_ff");
-    $this->db->join("origen_de_financiamiento of", "pre.origen_de_financiamiento_id_of = of.id_of");
-    $this->db->join("programa pr", "pre.programa_id_pro = pr.id_pro");
-    $this->db->join("cuentacontable c", "pre.IDCUENTACONTABLE = c.IDCuentaContable");
-	$this->db->join('uni_respon_usu', 'pre.id_uni_respon_usu = uni_respon_usu.id_uni_respon_usu');
-	$this->db->where('pre.estado', '1');
-	$this->db->where('uni_respon_usu.id_uni_respon_usu', $id_uni_respon_usu);
-	
-	$resultados = $this->db->get();
-	return $resultados->result();
-}
+class Presupuesto_model extends CI_Model
+{
 
 
 
+	public function getPresu($id_uni_respon_usu)
+	{
+		$this->db->select('pre.*, c.Descripcion_CC as descripcion, ff.nombre as fuente_de_financiamiento, of.nombre as origen_de_financiamiento, pr.nombre as programa');
+		$this->db->from('presupuestos pre');
+		$this->db->join("fuente_de_financiamiento ff", "pre.fuente_de_financiamiento_id_ff = ff.id_ff");
+		$this->db->join("origen_de_financiamiento of", "pre.origen_de_financiamiento_id_of = of.id_of");
+		$this->db->join("programa pr", "pre.programa_id_pro = pr.id_pro");
+		$this->db->join("cuentacontable c", "pre.IDCUENTACONTABLE = c.IDCuentaContable");
+		$this->db->join('uni_respon_usu', 'pre.id_uni_respon_usu = uni_respon_usu.id_uni_respon_usu');
+		$this->db->where('pre.estado', '1');
+		$this->db->where('uni_respon_usu.id_uni_respon_usu', $id_uni_respon_usu);
 
-	public function save($data){
-		return $this->db->insert("presupuestos",$data);
+		$resultados = $this->db->get();
+		return $resultados->result();
 	}
 
-	public function getPresupuesto($id){
-		$this->db->where("ID_Presupuesto",$id);
+
+
+
+	public function save($data)
+	{
+		return $this->db->insert("presupuestos", $data);
+	}
+
+	public function getPresupuesto($id)
+	{
+		$this->db->where("ID_Presupuesto", $id);
 		$resultado = $this->db->get("presupuestos");
 		return $resultado->row();
 
 	}
 
-	public function update($id, $data){
-		$this->db->where("id_presupuesto",$id);
-		return $this->db->update("presupuestos",$data);
+	public function update($id, $data)
+	{
+		$this->db->where("id_presupuesto", $id);
+		return $this->db->update("presupuestos", $data);
 	}
-
-
+	public function sumarDebePorMes($mes, $idCuentaContable, $idOf, $idFf, $idPro) {
+		$this->db->select('MONTH(na.FechaEmision) as mes, SUM(nad.Debe) as suma_debe');
+		$this->db->from('num_asi_deta as nad');
+		$this->db->join('num_asi as na', 'nad.Num_Asi_IDNum_Asi = na.IDNum_Asi');
+		$this->db->where('MONTH(na.FechaEmision)', $mes);
+		$this->db->where('nad.IDCuentaContable', $idCuentaContable);
+		$this->db->where('nad.id_of', $idOf);
+		$this->db->where('nad.id_ff', $idFf);
+		$this->db->where('nad.id_pro', $idPro);
+		$this->db->group_by('MONTH(na.FechaEmision)');
+	
+		$query = $this->db->get();
+		return $query->result();
+	}
+	public function obtenerDatosPresupuestoAnterior($idOf, $idFf, $idPro, $idCuentaContable, $mesActual) {
+		// Calculamos el mes anterior al mes actual
+		$mesAnterior = $mesActual - 1;
+		if ($mesAnterior < 1) {
+			$mesAnterior = 12;
+		}
+	
+		// Buscamos el presupuesto correspondiente al mes anterior
+		$this->db->select('pre_ene, pre_feb, pre_mar, pre_abr, pre_may, pre_jun, pre_jul, pre_ago, pre_sep, pre_oct, pre_nov, pre_dic');
+		$this->db->from('presupuestos');
+		$this->db->where('origen_de_financiamiento_id_of', $idOf);
+		$this->db->where('fuente_de_financiamiento_id_ff', $idFf);
+		$this->db->where('programa_id_pro', $idPro);
+		$this->db->where('Idcuentacontable', $idCuentaContable);
+		$this->db->where('estado', '1');
+		$query = $this->db->get();
+		$resultado = $query->row();
+	
+		// Obtenemos el valor correspondiente al mes anterior
+		$valorMesAnterior = $resultado->{'pre_' . $this->obtenerNombreMes($mesAnterior)};
+		
+		return $valorMesAnterior;
+	}
+	
+	private function obtenerNombreMes($numeroMes) {
+		// Array con los nombres de los meses
+		$nombresMeses = array(
+			1 => 'ene', 2 => 'feb', 3 => 'mar', 4 => 'abr',
+			5 => 'may', 6 => 'jun', 7 => 'jul', 8 => 'ago',
+			9 => 'sep', 10 => 'oct', 11 => 'nov', 12 => 'dic'
+		);
+		// Retornamos el nombre del mes correspondiente al número
+		return $nombresMeses[$numeroMes];
+	}
+	
+	
 	
 }
