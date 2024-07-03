e9006826634c34abaddb1f20915bc18e241cce29
izok999 <isaacbritez99@gmail.com>
2024-07-03 10:25:54 -0400

    re merge

--- application/models/Bienes_Servicios_model.php
+++ application/models/Bienes_Servicios_model.php
@@ -1,40 +1,4 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Bienes_Servicios_model extends CI_Model {
-
-	public function getBienesServicios($id_uni_respon_usu) {
-		$this->db->select('bienes_servicios.*');
-		$this->db->from('bienes_servicios');
-		$this->db->join('uni_respon_usu', 'bienes_servicios.id_uni_respon_usu = uni_respon_usu.id_uni_respon_usu');
-		$this->db->where('bienes_servicios.estado', '1');
-		$this->db->where('uni_respon_usu.id_uni_respon_usu', $id_uni_respon_usu);
-		
-		$resultados = $this->db->get();
-		return $resultados->result();
-	}
-	
-
-	
-	public function save($data) {
-	
-		return $this->db->insert('bienes_servicios', $data);
-	}
-	
-	
-    
-	public function getBienServicio($id){
-		$this->db->where("IDbienservicio",$id);
-		$resultado = $this->db->get("bienes_servicios");
-		return $resultado->row();
-
-	}
- 
-	public function update($id,$data){
-		$this->db->where("IDbienservicio",$id);
-		return $this->db->update("bienes_servicios",$data);
-	}
-}
+-
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
