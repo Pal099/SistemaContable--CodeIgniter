930528c29146175d19b8b8056e1b4fbdd84ad56a
izok999 <isaacbritez99@gmail.com>
2024-09-19 21:39:59 -0400

    modificado para red local de sol consultora

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '192.168.100.158',
 	'username' => 'root',
 	'password' => '', 
 	'database' => 'contanuevo',
