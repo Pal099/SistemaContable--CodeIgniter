909ac6ed9816d2756d88b54b95fc5c5ccd1a03eb
izok999 <isaacbritez99@gmail.com>
2025-02-10 22:40:04 -0300

    Arreglé los modelos de comprobante de gasto y presupuesto para que funcione con la bd nueva

--- application/models/Presupuesto_model.php
+++ application/models/Presupuesto_model.php
@@ -4,16 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Presupuesto_model extends CI_Model {
 
 
-
     public function getPresu($id_uni_respon_usu) {
-        $this->db->select('pre.*, c.Descripcion_CC as descripcion, ff.nombre as fuente_de_financiamiento, of.nombre as origen_de_financiamiento, pr.nombre as programa, pf.*');
+        $this->db->select('pre_men.*, pre.*, c.Descripcion_CC as descripcion, ff.nombre as fuente_de_financiamiento, of.nombre as origen_de_financiamiento, pr.nombre as programa');
         $this->db->from('presupuestos pre');
         $this->db->join("fuente_de_financiamiento ff", "pre.fuente_de_financiamiento_id_ff = ff.id_ff");
         $this->db->join("origen_de_financiamiento of", "pre.origen_de_financiamiento_id_of = of.id_of");
         $this->db->join("programa pr", "pre.programa_id_pro = pr.id_pro");
         $this->db->join("cuentacontable c", "pre.Idcuentacontable = c.IDCuentaContable");
+        $this->db->join("presupuesto_mensual pre_men", "pre.ID_Presupuesto = pre_men.id_presupuesto");
         $this->db->join('uni_respon_usu', 'pre.id_uni_respon_usu = uni_respon_usu.id_uni_respon_usu');
-        $this->db->join('plan_financiero pf', 'pre.id_pf_fk = pf.ID_PF'); // Unión externa izquierda
         $this->db->where('pre.estado', '1');
         $this->db->where('uni_respon_usu.id_uni_respon_usu', $id_uni_respon_usu);
         
