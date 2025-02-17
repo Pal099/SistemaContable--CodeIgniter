7926398eb59464cdaeeb9faa9a40017c689ad5f8
Adrian-Portillo <adrnnnx@hotmail.com>
2025-02-17 18:48:00 -0300

    Primer commi nueva rama

---
+++ application/logs/log-2025-02-17.php
@@ -0,0 +1,143 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2025-02-17 22:45:23 --> Config Class Initialized
+INFO - 2025-02-17 22:45:23 --> Hooks Class Initialized
+DEBUG - 2025-02-17 22:45:24 --> UTF-8 Support Enabled
+INFO - 2025-02-17 22:45:24 --> Utf8 Class Initialized
+INFO - 2025-02-17 22:45:24 --> URI Class Initialized
+INFO - 2025-02-17 22:45:25 --> Router Class Initialized
+INFO - 2025-02-17 22:45:25 --> Output Class Initialized
+INFO - 2025-02-17 22:45:25 --> Security Class Initialized
+DEBUG - 2025-02-17 22:45:25 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-17 22:45:25 --> Input Class Initialized
+INFO - 2025-02-17 22:45:25 --> Language Class Initialized
+INFO - 2025-02-17 22:45:26 --> Loader Class Initialized
+INFO - 2025-02-17 22:45:26 --> Helper loaded: url_helper
+INFO - 2025-02-17 22:45:27 --> Database Driver Class Initialized
+DEBUG - 2025-02-17 22:45:27 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2025-02-17 22:45:27 --> Session: Class initialized using 'files' driver.
+INFO - 2025-02-17 22:45:27 --> Helper loaded: form_helper
+INFO - 2025-02-17 22:45:27 --> Form Validation Class Initialized
+INFO - 2025-02-17 22:45:27 --> Controller Class Initialized
+INFO - 2025-02-17 22:45:27 --> Helper loaded: inflector_helper
+INFO - 2025-02-17 22:45:28 --> Model Class Initialized
+INFO - 2025-02-17 22:45:28 --> Model Class Initialized
+INFO - 2025-02-17 22:45:28 --> Model Class Initialized
+DEBUG - 2025-02-17 22:45:28 --> Session class already loaded. Second attempt ignored.
+INFO - 2025-02-17 22:45:28 --> Model Class Initialized
+INFO - 2025-02-17 22:45:28 --> Model Class Initialized
+INFO - 2025-02-17 22:45:28 --> Model Class Initialized
+INFO - 2025-02-17 22:45:28 --> Model Class Initialized
+DEBUG - 2025-02-17 22:45:28 --> Form_validation class already loaded. Second attempt ignored.
+INFO - 2025-02-17 22:45:29 --> File loaded: C:\xampp\htdocs\practica\application\views\layouts/header.php
+INFO - 2025-02-17 22:45:29 --> File loaded: C:\xampp\htdocs\practica\application\views\layouts/sideBar.php
+INFO - 2025-02-17 22:45:30 --> File loaded: C:\xampp\htdocs\practica\application\views\admin/comprobantegasto/add.php
+INFO - 2025-02-17 22:45:30 --> File loaded: C:\xampp\htdocs\practica\application\views\layouts/footer.php
+INFO - 2025-02-17 22:45:30 --> Final output sent to browser
+DEBUG - 2025-02-17 22:45:30 --> Total execution time: 8.2935
+INFO - 2025-02-17 22:45:55 --> Config Class Initialized
+INFO - 2025-02-17 22:45:55 --> Hooks Class Initialized
+DEBUG - 2025-02-17 22:45:55 --> UTF-8 Support Enabled
+INFO - 2025-02-17 22:45:55 --> Utf8 Class Initialized
+INFO - 2025-02-17 22:45:55 --> URI Class Initialized
+INFO - 2025-02-17 22:45:55 --> Router Class Initialized
+INFO - 2025-02-17 22:45:55 --> Output Class Initialized
+INFO - 2025-02-17 22:45:55 --> Security Class Initialized
+DEBUG - 2025-02-17 22:45:55 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-17 22:45:55 --> Input Class Initialized
+INFO - 2025-02-17 22:45:55 --> Language Class Initialized
+INFO - 2025-02-17 22:45:55 --> Loader Class Initialized
+INFO - 2025-02-17 22:45:55 --> Helper loaded: url_helper
+INFO - 2025-02-17 22:45:55 --> Database Driver Class Initialized
+DEBUG - 2025-02-17 22:45:55 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2025-02-17 22:45:55 --> Session: Class initialized using 'files' driver.
+INFO - 2025-02-17 22:45:55 --> Helper loaded: form_helper
+INFO - 2025-02-17 22:45:55 --> Form Validation Class Initialized
+INFO - 2025-02-17 22:45:55 --> Controller Class Initialized
+INFO - 2025-02-17 22:45:55 --> Helper loaded: inflector_helper
+INFO - 2025-02-17 22:45:55 --> Model Class Initialized
+INFO - 2025-02-17 22:45:55 --> Model Class Initialized
+INFO - 2025-02-17 22:45:55 --> Model Class Initialized
+DEBUG - 2025-02-17 22:45:55 --> Session class already loaded. Second attempt ignored.
+INFO - 2025-02-17 22:45:55 --> Model Class Initialized
+INFO - 2025-02-17 22:45:55 --> Model Class Initialized
+INFO - 2025-02-17 22:45:55 --> Model Class Initialized
+INFO - 2025-02-17 22:45:55 --> Model Class Initialized
+DEBUG - 2025-02-17 22:45:55 --> Form_validation class already loaded. Second attempt ignored.
+INFO - 2025-02-17 22:45:55 --> File loaded: C:\xampp\htdocs\practica\application\views\layouts/header.php
+INFO - 2025-02-17 22:45:55 --> File loaded: C:\xampp\htdocs\practica\application\views\layouts/sideBar.php
+INFO - 2025-02-17 22:45:56 --> File loaded: C:\xampp\htdocs\practica\application\views\admin/comprobantegasto/list.php
+INFO - 2025-02-17 22:45:56 --> File loaded: C:\xampp\htdocs\practica\application\views\layouts/footer.php
+INFO - 2025-02-17 22:45:56 --> Final output sent to browser
+DEBUG - 2025-02-17 22:45:56 --> Total execution time: 0.8071
+INFO - 2025-02-17 22:45:57 --> Config Class Initialized
+INFO - 2025-02-17 22:45:57 --> Hooks Class Initialized
+DEBUG - 2025-02-17 22:45:57 --> UTF-8 Support Enabled
+INFO - 2025-02-17 22:45:57 --> Utf8 Class Initialized
+INFO - 2025-02-17 22:45:57 --> URI Class Initialized
+INFO - 2025-02-17 22:45:57 --> Router Class Initialized
+INFO - 2025-02-17 22:45:57 --> Output Class Initialized
+INFO - 2025-02-17 22:45:57 --> Security Class Initialized
+DEBUG - 2025-02-17 22:45:57 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-17 22:45:57 --> Input Class Initialized
+INFO - 2025-02-17 22:45:57 --> Language Class Initialized
+ERROR - 2025-02-17 22:45:58 --> 404 Page Not Found: Assets/json
+INFO - 2025-02-17 22:46:02 --> Config Class Initialized
+INFO - 2025-02-17 22:46:02 --> Hooks Class Initialized
+DEBUG - 2025-02-17 22:46:02 --> UTF-8 Support Enabled
+INFO - 2025-02-17 22:46:02 --> Utf8 Class Initialized
+INFO - 2025-02-17 22:46:02 --> URI Class Initialized
+INFO - 2025-02-17 22:46:02 --> Router Class Initialized
+INFO - 2025-02-17 22:46:02 --> Output Class Initialized
+INFO - 2025-02-17 22:46:02 --> Security Class Initialized
+DEBUG - 2025-02-17 22:46:02 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-17 22:46:02 --> Input Class Initialized
+INFO - 2025-02-17 22:46:02 --> Language Class Initialized
+INFO - 2025-02-17 22:46:02 --> Loader Class Initialized
+INFO - 2025-02-17 22:46:02 --> Helper loaded: url_helper
+INFO - 2025-02-17 22:46:02 --> Database Driver Class Initialized
+DEBUG - 2025-02-17 22:46:02 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2025-02-17 22:46:02 --> Session: Class initialized using 'files' driver.
+INFO - 2025-02-17 22:46:02 --> Helper loaded: form_helper
+INFO - 2025-02-17 22:46:02 --> Form Validation Class Initialized
+INFO - 2025-02-17 22:46:02 --> Controller Class Initialized
+INFO - 2025-02-17 22:46:02 --> Helper loaded: inflector_helper
+INFO - 2025-02-17 22:46:02 --> Model Class Initialized
+INFO - 2025-02-17 22:46:02 --> Model Class Initialized
+INFO - 2025-02-17 22:46:02 --> Model Class Initialized
+DEBUG - 2025-02-17 22:46:02 --> Session class already loaded. Second attempt ignored.
+INFO - 2025-02-17 22:46:02 --> Model Class Initialized
+INFO - 2025-02-17 22:46:02 --> Model Class Initialized
+INFO - 2025-02-17 22:46:02 --> Model Class Initialized
+INFO - 2025-02-17 22:46:02 --> Model Class Initialized
+DEBUG - 2025-02-17 22:46:02 --> Form_validation class already loaded. Second attempt ignored.
+INFO - 2025-02-17 22:46:03 --> File loaded: C:\xampp\htdocs\practica\application\views\layouts/header.php
+INFO - 2025-02-17 22:46:03 --> File loaded: C:\xampp\htdocs\practica\application\views\layouts/sideBar.php
+INFO - 2025-02-17 22:46:03 --> File loaded: C:\xampp\htdocs\practica\application\views\admin/comprobantegasto/add.php
+INFO - 2025-02-17 22:46:03 --> File loaded: C:\xampp\htdocs\practica\application\views\layouts/footer.php
+INFO - 2025-02-17 22:46:03 --> Final output sent to browser
+DEBUG - 2025-02-17 22:46:03 --> Total execution time: 0.6674
+INFO - 2025-02-17 22:46:55 --> Config Class Initialized
+INFO - 2025-02-17 22:46:55 --> Hooks Class Initialized
+DEBUG - 2025-02-17 22:46:55 --> UTF-8 Support Enabled
+INFO - 2025-02-17 22:46:55 --> Utf8 Class Initialized
+INFO - 2025-02-17 22:46:55 --> URI Class Initialized
+INFO - 2025-02-17 22:46:55 --> Router Class Initialized
+INFO - 2025-02-17 22:46:55 --> Output Class Initialized
+INFO - 2025-02-17 22:46:55 --> Security Class Initialized
+DEBUG - 2025-02-17 22:46:55 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-17 22:46:55 --> Input Class Initialized
+INFO - 2025-02-17 22:46:55 --> Language Class Initialized
+ERROR - 2025-02-17 22:46:55 --> 404 Page Not Found: Comprobante_gasto/verificar_saldo
+INFO - 2025-02-17 22:46:58 --> Config Class Initialized
+INFO - 2025-02-17 22:46:58 --> Hooks Class Initialized
+DEBUG - 2025-02-17 22:46:58 --> UTF-8 Support Enabled
+INFO - 2025-02-17 22:46:58 --> Utf8 Class Initialized
+INFO - 2025-02-17 22:46:58 --> URI Class Initialized
+INFO - 2025-02-17 22:46:58 --> Router Class Initialized
+INFO - 2025-02-17 22:46:58 --> Output Class Initialized
+INFO - 2025-02-17 22:46:58 --> Security Class Initialized
+DEBUG - 2025-02-17 22:46:58 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-02-17 22:46:58 --> Input Class Initialized
+INFO - 2025-02-17 22:46:58 --> Language Class Initialized
+ERROR - 2025-02-17 22:46:58 --> 404 Page Not Found: Comprobante_gasto/verificar_saldo
