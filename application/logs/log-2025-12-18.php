7ff589d569e2d564e112b1ef98ef7382c2eec9bd
izak999 <isaacbritez99@gmail.com>
2025-12-18 20:13:41 -0300

    Guardamos la versión actual de comprobante de gastos,hecho en laragon, haremos merge con la master y la de docker

---
+++ application/logs/log-2025-12-18.php
@@ -0,0 +1,43 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2025-12-18 23:06:54 --> UTF-8 Support Enabled
+DEBUG - 2025-12-18 23:06:54 --> No URI present. Default controller set.
+DEBUG - 2025-12-18 23:06:54 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2025-12-18 23:06:55 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2025-12-18 23:06:56 --> Form_validation class already loaded. Second attempt ignored.
+DEBUG - 2025-12-18 23:06:56 --> Total execution time: 2.8070
+DEBUG - 2025-12-18 23:07:03 --> UTF-8 Support Enabled
+DEBUG - 2025-12-18 23:07:03 --> No URI present. Default controller set.
+DEBUG - 2025-12-18 23:07:03 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2025-12-18 23:07:03 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2025-12-18 23:07:03 --> Form_validation class already loaded. Second attempt ignored.
+ERROR - 2025-12-18 23:07:03 --> Severity: Warning --> Undefined property: stdClass::$unidad C:\laragon\www\practica\application\controllers\Login.php 40
+DEBUG - 2025-12-18 23:07:03 --> UTF-8 Support Enabled
+DEBUG - 2025-12-18 23:07:03 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2025-12-18 23:07:03 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2025-12-18 23:07:05 --> Total execution time: 1.3559
+DEBUG - 2025-12-18 23:07:09 --> UTF-8 Support Enabled
+DEBUG - 2025-12-18 23:07:09 --> No URI present. Default controller set.
+DEBUG - 2025-12-18 23:07:09 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2025-12-18 23:07:09 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2025-12-18 23:07:09 --> Form_validation class already loaded. Second attempt ignored.
+DEBUG - 2025-12-18 23:07:09 --> Total execution time: 0.0721
+DEBUG - 2025-12-18 23:07:22 --> UTF-8 Support Enabled
+DEBUG - 2025-12-18 23:07:22 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2025-12-18 23:07:22 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2025-12-18 23:07:22 --> Total execution time: 0.1214
+DEBUG - 2025-12-18 23:07:43 --> UTF-8 Support Enabled
+DEBUG - 2025-12-18 23:07:43 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2025-12-18 23:07:43 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2025-12-18 23:07:44 --> Form_validation class already loaded. Second attempt ignored.
+ERROR - 2025-12-18 23:07:44 --> Severity: Warning --> Undefined variable $op_siguiente C:\laragon\www\practica\application\views\admin\obligacion\obli_combined.php 201
+DEBUG - 2025-12-18 23:07:44 --> Total execution time: 1.4112
+DEBUG - 2025-12-18 23:07:53 --> UTF-8 Support Enabled
+DEBUG - 2025-12-18 23:07:53 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2025-12-18 23:07:53 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2025-12-18 23:07:54 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2025-12-18 23:07:54 --> Form_validation class already loaded. Second attempt ignored.
+DEBUG - 2025-12-18 23:07:54 --> Total execution time: 0.4477
+DEBUG - 2025-12-18 23:07:55 --> UTF-8 Support Enabled
+DEBUG - 2025-12-18 23:07:55 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2025-12-18 23:07:55 --> 404 Page Not Found: Assets/json
